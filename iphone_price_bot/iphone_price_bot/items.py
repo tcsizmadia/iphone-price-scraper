0cdf787dd677c22dfc268ec872b095c7d4275e06
Tamas Csizmadia <tamas@me.com>
2024-03-09 22:43:36 +0100

    chore: refactor spider logic
    Change IphonePricesItem to use spider_utils methods via MapCompose
    Define Pipelines for more sophisticated manipulation of scraped iPhone price data
    Add Pipelines and feed format settings to settings.py
    Add spider_utils.py with helper functions such us translate colors from Swedish or Hungarian to English and clean data.
    Refactor AppleWebsiteSpider

--- iphone_price_bot/iphone_price_bot/items.py
+++ iphone_price_bot/iphone_price_bot/items.py
@@ -4,11 +4,15 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose
+from . import spider_utils
 
 
 class IphonePricesItem(scrapy.Item):
-    country = scrapy.Field()
-    model = scrapy.Field()
-    capacity = scrapy.Field()
+
+    country = scrapy.Field(input_processor=MapCompose(spider_utils.get_country_code))
+    model = scrapy.Field(input_processor=MapCompose(spider_utils.clean_model))
+    capacity = scrapy.Field(input_processor=MapCompose(spider_utils.clean_value))
+    capacity_unit = scrapy.Field()
     color = scrapy.Field()
-    price = scrapy.Field()
+    price = scrapy.Field(input_processor=MapCompose(spider_utils.clean_value))
